7e97cf0b815e736819ddf28856749975659091eb
Reda Ouadi <redaouadi34@gmail.com>
2024-08-13 23:42:58 +0100

    changer  la configuration de  security

--- equipment-management/src/main/java/com/itsolutions/equipment_management/models/Panne.java
+++ equipment-management/src/main/java/com/itsolutions/equipment_management/models/Panne.java
@@ -1,5 +1,11 @@
 package com.itsolutions.equipment_management.models;
+import java.util.List;
+import java.util.Optional;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.itsolutions.equipment_management.repositories.EquipmentRepository;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,7 +13,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -15,6 +23,8 @@ import java.util.Set;
 @AllArgsConstructor
 @Entity
 @Table(name = "panne")
+@JsonIgnoreProperties(ignoreUnknown = true)
+
 public class Panne {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +37,38 @@ public class Panne {
     @Enumerated(EnumType.STRING)
     private EtatPanne etatPanne;
 
-    @ManyToMany(mappedBy = "pannes")
-    private Set<Equipment> equipments;
+    @Transient
+    private Set<Long> equipmentIds = new HashSet<>();
+
+    @JsonManagedReference(value = "panne-tickets")
+    @OneToMany(mappedBy = "panne")
+    private List<Ticket> tickets;
+    @ManyToMany
+    @JoinTable(
+            name = "panne_equipment",
+            joinColumns = @JoinColumn(name = "panne_id"),
+            inverseJoinColumns = @JoinColumn(name = "equipment_id")
+    )
+    private Set<Equipment> equipments = new HashSet<>();
+
+    @Transient
+    public void setEquipmentsFromIds(Set<Long> equipmentIds, EquipmentRepository equipmentRepository) {
+        this.equipments = equipmentIds.stream()
+                .map(equipmentRepository::findById)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet());
+    }
+    @Transient
+    public Set<Long> getEquipmentsIds() {
+        return equipments.stream()
+                .map(Equipment::getId)
+                .collect(Collectors.toSet());
+    }
+
+    public LocalDateTime getDate() {
+        return  this.datePanne;
+    }
 }
+
+
