361627a7b9186688f63bcb8b586ea092133e4512
Reda Ouadi <redaouadi34@gmail.com>
2024-08-13 23:37:42 +0100

    add Conception

--- equipment-management/src/main/java/com/itsolutions/equipment_management/repositories/TechnicienRepository.java
+++
@@ -1,7 +0,0 @@
-package com.itsolutions.equipment_management.repositories;
-
-import com.itsolutions.equipment_management.models.Technicien;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechnicienRepository extends JpaRepository<Technicien, Long> {
-}
--- equipment-management/src/main/java/com/itsolutions/equipment_management/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.itsolutions.equipment_management.repositories;
-
-import com.itsolutions.equipment_management.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
